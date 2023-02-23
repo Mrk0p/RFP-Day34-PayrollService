578de206594e5dd7d35a618a2d33d598be2433a5
Mrk0p <sachinwele23@gmail.com>
2023-02-23 13:01:32 +0530

    Added:update the salary in database

--- src/main/java/com/bridgelabz/payroll_service/EmployeeCustomException.java
+++ src/main/java/com/bridgelabz/payroll_service/EmployeeCustomException.java
@@ -1,7 +1,6 @@
 package com.bridgelabz.payroll_service;
 
 public class EmployeeCustomException extends Exception {
-
     public EmployeeCustomException(String Invalid_Input)
     {
         super(Invalid_Input);
